6a30a18eb2c9bab96c248096ef822b08d1419720
Prometheus <105338051+AlRzRz@users.noreply.github.com>
2025-09-18 19:54:28 +0400

    Add TODO's and content_script matching

--- background.js
+++
@@ -1,2 +0,0 @@
-// [ ] Add icon 32 and icon 16 images
-// [ ] Remove background from icon images
---
+++ scripts/content.js
@@ -0,0 +1,20 @@
+// [ ] Add icon 32 and icon 16 images
+// [ ] Remove background from icon images
+
+// https://www.chess.com/play/
+// https://www.chess.com/play/*
+
+// #board-layout-player-top > div.player-component.player-top (The Opponent)
+// #board-layout-player-bottom > div.player-component.player-bottom (The User)
+
+// Avatars controlled by img's in divs: div.player-avatar
+// Other information controlled by elements in divs: div.player-tagline
+
+// [ ] Current Implementation: Hide taglines and standardize player avatars
+// [ ] Instead of hiding taglines, have a way to render 'Opponent' or 'User' instead.
+
+
+
+function hideMainDetails(documentItem) {
+    
+}
