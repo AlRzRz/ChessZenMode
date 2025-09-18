c09d4c3b6e1100857f067880df833e0bb7f519e6
Prometheus <105338051+AlRzRz@users.noreply.github.com>
2025-09-18 20:24:54 +0400

    V1.0.0 - Hide player taglines and standardize the opponent's image

--- scripts/content.js
+++ scripts/content.js
@@ -9,12 +9,40 @@
 
 // Avatars controlled by img's in divs: div.player-avatar
 // Other information controlled by elements in divs: div.player-tagline
+// Avatar img elements: img with a class of 'cc-avatar-img'
+// Avatar img div: #board-layout-player-top > div.player-component.player-top > div.player-avatar > div > img
+// Standardized Avatar: https://www.chess.com/bundles/web/images/black_400.png
+
 
 // [ ] Current Implementation: Hide taglines and standardize player avatars
 // [ ] Instead of hiding taglines, have a way to render 'Opponent' or 'User' instead.
 
 
 
-function hideMainDetails(documentItem) {
-    
+function hideMainDetails(documentElement) {
+
+    // [x] GET PLAYER ELEMENT TAGLINES, LOOP OVER THEM AND SET THEIR VISIBILITY TO HIDDEN
+
+    let taglines = documentElement.querySelectorAll('div.player-tagline');
+    taglines.forEach(tagline => {
+        tagline.style.visibility = 'hidden';
+    });
+
+
+    // [ ] GET PLAYER AVATARS AND CHANGE THEIR PICTURES TO A STANDARDIZED PICTURE (either get its parent container, empty it, and add a simple image)
+
+    const STANDARDIZED_IMAGE_ELEMENT = '<img class="cc-avatar-img" loading="lazy" src="https://www.chess.com/bundles/web/images/black_400.png" srcset="https://www.chess.com/bundles/web/images/black_400.png 1x, https://www.chess.com/bundles/web/images/black_400@2x.png 2x" alt="Avatar of Opponent" height="40" width="40"></img>'
+    let opponentImageDiv = document.querySelector('#board-layout-player-top > div.player-component.player-top > div.player-avatar > div');
+    opponentImageDiv.innerHTML = STANDARDIZED_IMAGE_ELEMENT;
+
+
+
 }
+
+
+const main = (documentElement) => {
+    hideMainDetails(documentElement)
+}
+
+
+main(document)
